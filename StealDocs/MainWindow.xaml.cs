3aa3a347f9ea1e6a0be729931b1629a55c61779f
Dwenn Kaufmann <Dwenn@live.fr>
2019-06-17 19:26:22 +0200

    First release, not working yet. see ya

--- StealDocs/MainWindow.xaml.cs
+++ StealDocs/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,70 @@ namespace StealDocs
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string sourceDirectory = @"C:\temp\source";
+            string targetDirectory = @"C:\temp\destination";
+
+            Copy(sourceDirectory, targetDirectory);
+        }
+
+        public static void Copy(string sourceDirectory, string targetDirectory)
+        {
+            var diSource = new DirectoryInfo(sourceDirectory);
+            var diTarget = new DirectoryInfo(targetDirectory);
+
+            CopyAll(diSource, diTarget);
+        }
+
+        public static void CopyAll(DirectoryInfo source, DirectoryInfo target)
+        {
+            Directory.CreateDirectory(target.FullName);
+
+            // Copy each file into the new directory.
+            foreach (FileInfo fi in source.GetFiles())
+            {
+                Console.WriteLine(@"Copying {0}\{1}", target.FullName, fi.Name);
+                fi.CopyTo(System.IO.Path.Combine(target.FullName, fi.Name), true);
+            }
+
+            // Copy each subdirectory using recursion.
+            foreach (DirectoryInfo diSourceSubDir in source.GetDirectories())
+            {
+                DirectoryInfo nextTargetSubDir =
+                    target.CreateSubdirectory(diSourceSubDir.Name);
+                CopyAll(diSourceSubDir, nextTargetSubDir);
+            }
+        }
+
+        public string getUserPath()
+        {
+            String UserPath = "";
+
+            switch (User.Items.CurrentItem)
+            {
+                case "Thierry": UserPath = "schedert";
+                    break;
+                case "Achilles": UserPath = "gontrana";
+                    break;
+                case "Remy": UserPath = "zimmermannr";
+                    break;
+                case "Maxence": UserPath = "benderm";
+                    break;
+                case "Dwenn": UserPath = "kaufmanndw";
+                    break;
+                case "Mehmet": UserPath = "onganm";
+                    break;
+                case "Dilane": UserPath = "rodriguesD";
+                    break;
+                default: UserPath = "%USERNAME%";
+                    break;
+            }
+ 
+
+            return UserPath;
         }
     }
 }
